1003c6a244456ace79ad5ea4075a2189eae1b310
Trestin <trestinishak@gmail.com>
2021-08-16 12:00:54 -0600

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector(`form`).addEventListener(`submit`, (form) => {
+    form.preventDefault();
+
+    const task = document.createElement(`p`);
+    task.textContent = form.target[`new-task-description`].value;
+
+    const button = document.createElement(`button`);
+    button.textContent = `x`;
+    button.addEventListener(`click`, (task) => task.target.parentNode.remove());
+
+    task.appendChild(button);
+    document.querySelector(`#tasks`).appendChild(task);
+  });
 });
